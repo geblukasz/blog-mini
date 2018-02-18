3d31d2b2c2beb9f5bfb4c0b8540d0846e32a1501
szkolenia_ak <szkolenia_ak@no-email.com>
2018-02-18 16:33:16 +0100

    next step

--- src/main/java/pl/akademiakodu/miniblog/controllers/TagRestController.java
+++ src/main/java/pl/akademiakodu/miniblog/controllers/TagRestController.java
@@ -1,16 +1,27 @@
 package pl.akademiakodu.miniblog.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.akademiakodu.miniblog.model.entities.Tag;
+import pl.akademiakodu.miniblog.model.repositories.TagRepository;
 
 @RestController
 public class TagRestController {
+
+    @Autowired
+    TagRepository tagRepository;
+
 
     @RequestMapping(value = "/tag", method = RequestMethod.POST)
     public ResponseEntity<Tag> createTag(@RequestParam String tagName){
         Tag tag = new Tag();
         tag.setTagName(tagName);
 
+        tagRepository.save(tag);
+
+        return ResponseEntity
+                .ok()
+                .body(tag);
     }
 }
