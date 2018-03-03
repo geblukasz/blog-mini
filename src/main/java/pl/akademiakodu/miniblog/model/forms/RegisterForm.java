e4790594ad2029081547c16a685bc1823fef69d8
szkolenia_ak <szkolenia_ak@no-email.com>
2018-03-03 16:16:26 +0100

    next step

--- src/main/java/pl/akademiakodu/miniblog/model/forms/RegisterForm.java
+++ src/main/java/pl/akademiakodu/miniblog/model/forms/RegisterForm.java
@@ -1,13 +1,14 @@
 package pl.akademiakodu.miniblog.model.forms;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class RegisterForm {
 
-    @Size(min = 6, message = "Nazwa uzytkownika musi posiadać minimum {min} znakow.")
+    @Size(min = 6, message = "Nazwa uzytkownika musi posiadać minimum {min} znakow. Podana fraza ${validatedValue} nie pasuje do tych wytycznych.")
     private String userName;
-    @NotBlank(message = "Email nie może być pusty.")
+    @Pattern(regexp = "[A-z0-9.]+@[a-z0-9\\-]+\\.[a-z]{2,5}", message = "Podaj prawidłowy email.")
     private String email;
     @Size(min = 6, max = 10, message = "Haslo musi miec pomiedzy {min} a {max} znakow.")
     private String password;
